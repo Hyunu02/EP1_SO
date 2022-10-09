b3605dd5b7cbc908138f1cc6921eb4a698cbd9d2
Andre Hyun <60765377+Hyunu02@users.noreply.github.com>
2022-10-08 22:42:46 -0300

    Update: Foram feitas muitas coisas
    Foram feitas muitas coisas

--- src/Escalonador.java
+++ src/Escalonador.java
@@ -2,23 +2,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.io.FileWriter;
+import java.io.FileOutputStream;
 
 public class Escalonador {
     public static void main(String[] args) {
         
-	// Iniciliza todos os processos na memoria e os coloca na tabela de 'prontos'.
-	for(int i = 1; i <= 10; i++) {
-		String path = "../programas/";
+    ArrayList <BCP> bcpTable = new ArrayList<BCP>();
 
+	// Inicializa todos os processos na memoria e os coloca na tabela de 'prontos'.
+	for(int i = 1; i <= 10; i++) {
+        String path = "programas/";
 		if(i < 10) {
 			path += "0" + i + ".txt";
-		}else {
+		} else {
 			path += i + ".txt";
 		}
 
 		File arq = new File(path);
-		File arqPrio = new File("../programas/prioridades.txt");
-		
+		File arqPrio = new File("programas/prioridades.txt");
 		// Lê o conteudo do arquivo e o associa a uma nova instancia de BCP.
 		try {
 			Scanner sc = new Scanner(arq);
@@ -30,15 +35,38 @@ public class Escalonador {
 
 				// Caso seja a primeira linha, cria a instancia de BCP e já atribui sua prioridade.
 				if(count == 0) {
+                    for(int j = 1; j < i; j++) {
+                        scPrio.nextLine();
+                    }
 					int prioridade = Integer.parseInt(scPrio.nextLine());
 					BCP processo = new BCP(data, prioridade);
+                    bcpTable.add(processo);
 				}
 				count++;	
 			}
 
-		}catch(FileNotFoundException e) {
-			System.out.println("Não foi possível ler o arquivo.");
+		}   catch (FileNotFoundException e) {
+		        System.out.println("Não foi possível ler o arquivo.");
+                e.printStackTrace();
+		    }
+	    }
+
+        Collections.sort(bcpTable, Comparator.comparing(BCP::getPrioridade));
+        Collections.reverse(bcpTable);
+        for(BCP b : bcpTable) {
+            System.out.println(b);
+        }
+        File arqQuant = new File("programas/quantum.txt");
+        int quantum = 0;
+        try {
+            Scanner sc = new Scanner(arqQuant);
+            quantum = Integer.parseInt(sc.nextLine());
+            System.out.println(quantum);
+            FileOutputStream fos = new FileOutputStream("OIEMAE.txt"); 
+        }
+        catch (FileNotFoundException e) {
+		        System.out.println("Não foi possível ler o arquivo.");
+                e.printStackTrace();
 		}
-	}
     }
 }
